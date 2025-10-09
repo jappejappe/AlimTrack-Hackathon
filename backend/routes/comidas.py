c43891fbf8f54b25dc8cdd633967ec36f95a352c
jappejappe <gabrieljappemoura95071@gmail.com>
2025-10-08 23:47:16 -0300

    feature: endpoint POST /cadastrar

--- backend/routes/comidas.py
+++ backend/routes/comidas.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+import json
 from backend.database.database import db
 
 
@@ -6,6 +7,67 @@ from backend.database.database import db
 # Cria um Blueprint do Flask para organizar as rotas de reservas.
 # O prefixo da URL para estas rotas será definido no app principal.
 comidas = Blueprint('comidas', __name__)
+
+@comidas.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    data = request.get_json(silent=True)
+
+    nome_consumidor = data.get('nome_consumidor')
+    matricula = data.get('matricula')
+    cpf_consumidor = data.get('cpf_consumidor')
+    senha = data.get('senha')
+    dias_padrao = data.get('dias_padrao')
+
+    if matricula:
+        # Usuário preencheu matrícula -> deve ter 10 caracteres
+        if len(matricula) != 10 or not matricula.isdigit():
+            return jsonify({
+                "status": "error",
+                "message": "Matrícula inválida"
+            }), 400
+
+    elif cpf_consumidor:
+        # Usuário preencheu CPF -> deve ter 11 caracteres
+        if len(cpf_consumidor) != 11 or not cpf_consumidor.isdigit():
+            return jsonify({
+                "status": "error",
+                "message": "CPF inválido"
+            }), 400
+
+    else:
+        # Nenhum campo preenchido
+        return jsonify({
+            "status": "error",
+            "message": "Informe a matrícula ou o CPF"
+        }), 400
+
+    # Executa a query para inserir o novo registro no banco de dados.
+    # O uso de parâmetros (%s) previne ataques de SQL Injection.
+    try:
+        db.query(
+            'INSERT INTO public.consumidores ' \
+            '(nome_consumidor, matricula, cpf_consumidor, senha, dias_padrao) ' \
+            'VALUES (%s, %s, %s, %s, %s);',
+            (
+                nome_consumidor,
+                matricula,
+                cpf_consumidor,
+                senha,
+                json.dumps(dias_padrao)
+            )
+        )
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": "Erro ao cadastrar consumidor. Detalhes: " + str(e)
+        }), 500
+
+    return jsonify({
+        "status": "success",
+        "message": "Consumidor cadastrado com sucesso!"
+    }), 201
+
 
 @comidas.route('/reservar', methods=['POST'])
 def reservar():
